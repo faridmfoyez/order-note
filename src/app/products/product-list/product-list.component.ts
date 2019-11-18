7f9686f54093f832c124f1a8089e8765ac8c40c5
Farid Mahmud <faridmfoyez@gmail.com>
2019-11-18 12:54:52 +0600

    add product list

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../product';
 
 @Component({
   selector: 'app-product-list',
@@ -6,6 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-list.component.scss']
 })
 export class ProductListComponent implements OnInit {
+
+  products = <Product[]>[
+    {
+      Id: 'muffin',
+      Name: 'Muffin',
+    },
+    {
+      Id: 'layer-cake',
+      Name: 'Layer Cake'
+    },
+    {
+      Id: 'swiss-roll',
+      Name: 'Swiss Roll'
+    },
+    {
+      Id: 'macaroons',
+      Name: 'Macaroons'
+    },
+  ];
 
   constructor() { }
 
--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list/product-list.component';
 import { RouterModule } from '@angular/router';
+import { MatCardModule, } from '@angular/material/card';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [ProductListComponent],
   imports: [
     CommonModule,
+    MatCardModule,
+    FlexLayoutModule,
     RouterModule.forChild([
       {
         path: '',
